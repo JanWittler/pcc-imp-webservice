b6c063ecc0e609c625b0ac74452124e945a405ea
Josh Romanowski <josh.romanowski@googlemail.com>
2017-01-18 13:57:48 +0100

    Add opencv

---
+++ src/test/java/edu/kit/informatik/pcc/service/videoprocessing/OpenCvTest.java
@@ -0,0 +1,25 @@
+package edu.kit.informatik.pcc.service.videoprocessing;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Josh Romanowski on 18.01.2017.
+ */
+public class OpenCvTest
+{
+    @BeforeClass
+    public static void setUp() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    @Test
+    public void OpenCvTets() {
+        Mat mat = new Mat();
+        assertNotNull(mat);
+    }
+}
