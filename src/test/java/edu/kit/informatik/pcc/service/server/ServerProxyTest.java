fc4d4be828eef56c94d4827876b0b7e6e19a0247
Fabian Wenzel <fabi.heisenberg@gmail.com>
2017-01-20 16:57:31 +0100

    change pom.xml /add test

---
+++ src/test/java/edu/kit/informatik/pcc/service/server/ServerProxyTest.java
@@ -0,0 +1,23 @@
+package edu.kit.informatik.pcc.service.server;
+
+import org.junit.*;
+
+/**
+ *
+ * @author Fabian Wenzel
+ * Created by Fabi on 20.01.2017.
+ */
+public class ServerProxyTest {
+    ServerProxy serverProxy;
+
+    @Before
+    public void setUp() {
+        serverProxy = new ServerProxy();
+    }
+
+    @org.junit.Test
+    public void setUpTest() {
+
+    }
+
+}
