d33589a738e556ec48d9a1c531cb604cc9ba1c30
Josh Romanowski <josh.romanowski@googlemail.com>
2017-01-20 12:43:16 +0100

    Add javadoc.

--- src/main/java/edu/kit/informatik/pcc/service/data/LocationConfig.java
+++ src/main/java/edu/kit/informatik/pcc/service/data/LocationConfig.java
@@ -7,34 +7,34 @@ package edu.kit.informatik.pcc.service.data;
  */
 public class LocationConfig {
 
-	// attributes
+    // attributes
 
-	/**
-	 * Project directory
-	 */
-	private static final String PROJECT_DIR = System.getProperty("user.dir");
-	/**
-	 * Directory for the anonymized videos.
-	 */
-	public static final String ANONYM_VID_DIR = PROJECT_DIR + "\\vid";
-	/**
-	 * Directory for the metadata.
-	 */
-	public static final String META_DIR = PROJECT_DIR + "\\meta";
-	/**
-	 * Directory for all temporary data.
-	 */
-	public static final String TEMP_DIR = PROJECT_DIR + "\\tmp";
-	/**
-	 * Directory for project resources.
-	 */
-	public static final String RESOURCES_DIR = PROJECT_DIR + "\\src\\main\\resources";
-	/**
-	 * Directory for test resources.
-	 */
-	public static final String TEST_RESOURCES_DIR = PROJECT_DIR + "\\src\\test\\resources";
-	/**
-	 * Directory for output data.
-	 */
-	public static final String OUTPUT_DIR = PROJECT_DIR + "\\target";
+    /**
+     * Project directory
+     */
+    private static final String PROJECT_DIR = System.getProperty("user.dir");
+    /**
+     * Directory for the anonymized videos.
+     */
+    public static final String ANONYM_VID_DIR = PROJECT_DIR + "\\vid";
+    /**
+     * Directory for the metadata.
+     */
+    public static final String META_DIR = PROJECT_DIR + "\\meta";
+    /**
+     * Directory for all temporary data.
+     */
+    public static final String TEMP_DIR = PROJECT_DIR + "\\tmp";
+    /**
+     * Directory for project resources.
+     */
+    public static final String RESOURCES_DIR = PROJECT_DIR + "\\src\\main\\resources";
+    /**
+     * Directory for test resources.
+     */
+    public static final String TEST_RESOURCES_DIR = PROJECT_DIR + "\\src\\test\\resources";
+    /**
+     * Directory for output data.
+     */
+    public static final String OUTPUT_DIR = PROJECT_DIR + "\\target";
 }
