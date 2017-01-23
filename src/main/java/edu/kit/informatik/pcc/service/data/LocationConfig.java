8a2cbe72119f950313f2ef2b4234012d26b3e369
David Laubenstein <david.laubenstein@gmail.com>
2017-01-23 11:43:30 +0100

    change all file seperators \\ to File.seperator, to use it in all operating systems

--- src/main/java/edu/kit/informatik/pcc/service/data/LocationConfig.java
+++ src/main/java/edu/kit/informatik/pcc/service/data/LocationConfig.java
@@ -1,5 +1,7 @@
 package edu.kit.informatik.pcc.service.data;
 
+import java.io.File;
+
 /**
  * Config class that holds all directories used.
  *
@@ -17,25 +19,25 @@ public class LocationConfig {
     /**
      * Directory for the anonymized videos.
      */
-    public static final String ANONYM_VID_DIR = PROJECT_DIR + "\\vid";
+    public static final String ANONYM_VID_DIR = PROJECT_DIR + File.separator + "vid";
     /**
      * Directory for the metadata.
      */
-    public static final String META_DIR = PROJECT_DIR + "\\meta";
+    public static final String META_DIR = PROJECT_DIR + File.separator + "meta";
     /**
      * Directory for all temporary data.
      */
-    public static final String TEMP_DIR = PROJECT_DIR + "\\tmp";
+    public static final String TEMP_DIR = PROJECT_DIR + File.separator + "tmp";
     /**
      * Directory for project resources.
      */
-    public static final String RESOURCES_DIR = PROJECT_DIR + "\\src\\main\\resources";
+    public static final String RESOURCES_DIR = PROJECT_DIR + File.separator + "src" + File.separator + "main" + File.separator + "resources";
     /**
      * Directory for test resources.
      */
-    public static final String TEST_RESOURCES_DIR = PROJECT_DIR + "\\src\\test\\resources";
+    public static final String TEST_RESOURCES_DIR = PROJECT_DIR + File.separator + "src" + File.separator + "test" + File.separator + "resources";
     /**
      * Directory for output data.
      */
-    public static final String OUTPUT_DIR = PROJECT_DIR + "\\target";
+    public static final String OUTPUT_DIR = PROJECT_DIR + File.separator + "target";
 }
