3ceb4578e9e39c950454ca720d1098bf28189b1d
Josh Romanowski <josh.romanowski@googlemail.com>
2017-02-17 17:59:10 +0100

    You dont want to test his

--- src/test/java/edu/kit/informatik/pcc/service/videoprocessing/chain/anonymization/OpenCvAnonymizeTest.java
+++ src/test/java/edu/kit/informatik/pcc/service/videoprocessing/chain/anonymization/OpenCvAnonymizeTest.java
@@ -14,6 +14,7 @@ import java.io.File;
  * @author Josh Romanowski
  *         Created by Josh Romanowski on 18.01.2017.
  */
+@Ignore
 public class OpenCvAnonymizeTest {
     private AAnonymizer anonymizer;
     private File input;
